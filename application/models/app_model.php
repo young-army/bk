d728c92df35ce80b05df8ab0919b0c831425ddc3
old-army <muhief.muhfianto@gmail.com>
2015-04-13 21:24:52 +0700

    massage

--- application/models/app_model.php
+++ application/models/app_model.php
@@ -53,6 +53,8 @@ class App_model extends CI_Model{
 		$gambar = $this->input->post('image_name');
 	}
 	
+	//MODEL EXPERTISE
+	
 	function expertise()
 	{
 		$query=$this->db->query("select * from expertise")->result();
@@ -69,11 +71,33 @@ class App_model extends CI_Model{
 		'judul' => $judul,
 		'author' => $author,
 		'keterangan' => $uraian,
-		'gambar' => $gambar,
 		'status' => $status
 		);
-		$this->db->insert('services',$data);
+		$this->db->insert('expertise',$data);
 	}
+	
+	function delete_expertise($id)
+	{
+		$this->db->where('id',$id);
+		$this->db->delete('expertise');
+	}
+	
+	function edit_expertise($id){
+		$query = $this->db->query("select * from expertise where id='$id'")->row($id);
+		return $query;
+	}
+	
+	function update_expertise($id)
+	{
+		$judul = $this->input->post('judul');
+		$author = $this->input->post('author');
+		$uraian = $this->input->post('uraian');
+		$status = $this->input->post('status');
+	
+		$this->db->query("update expertise set judul='$judul',author='$author',keterangan='$uraian',status='$status' where id='$id'");
+	}
+	
+	//END EXPERTISE
 	
 	function delete_services($id){
 		$this->db->where('id',$id);
@@ -96,24 +120,5 @@ class App_model extends CI_Model{
 	}
 
 
-	function delete_expertise($id)
-	{
-		$this->db->where('id',$id);
-		$this->db->delete('expertise');
-	}
 	
-	function edit_expertise($id){
-		$query = $this->db->query("select * from expertise where id='$id'")->row($id);
-		return $query;
-	}
-	
-	function update_expertise($id)
-	{
-		$judul = $this->input->post('judul');
-		$author = $this->input->post('author');
-		$uraian = $this->input->post('uraian');
-		$status = $this->input->post('status');
-	
-		$this->db->query("update expertise set judul='$judul',author='$author',keterangan='$uraian',status='$status' where id='$id'");
-	}
 }
